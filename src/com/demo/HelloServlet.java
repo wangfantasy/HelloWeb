e81fc6748b8006d279c684161c9401d732de7a8d
wangfantasy <201770110563@smail.hunnu.edu.cn>
2017-10-19 16:11:12 +0800

    Signed-off-by: wangfantasy <201770110563@smail.hunnu.edu.cn>

---
+++ src/com/demo/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().println("Hello, world!");
+		response.getWriter().println(new java.util.Date());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
